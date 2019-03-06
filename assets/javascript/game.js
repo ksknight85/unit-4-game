81392332dcff7f4cdd323cb2360994d2f95c03f0
ksknight <korvette_00Whotmail.com>
2019-03-05 17:13:13 -0700

    Updated code 3-5-19

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,89 @@
 // VARIABLES
 var targetScore;
 var yourScore = 0;
-var crystalValue = [];
+var crystalValArray = [];
+var totalCrystals = 4;
 var wins = 0;
 var losses = 0;
+var value;
+var imageCrystal;
 
 
 
 // FUNCTIONS
 // Target score
-function targetScore() {
+function setGame() {
     targetScore = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-}
+    $("#target-score-text").text("Target Score: " + targetScore);
+
+
 // Crystal Values
 // Determine the random values of 4 crystals
-for (i = 0; i < 4; i++) {
-    var value = [Math.floor(Math.random() * ((12 - 1) + 1) + 1)]
-    crystalValue.push(value);
+    for (i = 0; i < totalCrystals; i++) {
+        value = [Math.floor(Math.random() * ((12 - 1) + 1) + 1)];
+        crystalValArray.push(value);
+    }
+
+    // creates img and sets value of the crystals previously defined
+    for (var i = 0; i < crystalValArray.length; i++) {
+        imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "./assets/images/crystal" + [i + 1] + ".jpg");
+        imageCrystal.attr("data-crystalvalue", crystalValArray[i]);
+        $("#crystals").append(imageCrystal);
+    }
+}
+// Check score function
+function checkScore() {
+    if (yourScore === targetScore) {
+        win();
+    }
+    else if (yourScore > targetScore) {
+        lose();
+    }
 }
 
-// creates img and sets value of the crystals previously defined
-for (var i = 0; i < crystalValue.length; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "./assets/images/crystal1.jpg");
-    imageCrystal.attr("data-crystalvalue", crystalValue[i]);
-    $("#crystals").append(imageCrystal);
+// Win fuction
+function win() {
+    wins++;
+    $("#wins-text").text("Wins: " + wins);
+    yourScore = 0;
+    alert("You Win!!");
+    reset();
 }
+// Lose function
+function lose() {
+    losses++;
+    $("#loss-text").text("Losses: " + losses);
+    yourScore = 0;
+    alert("You lose");
+    reset();
+}
+
+// Reset Function
+function reset() {
+    yourScore = 0;
+    crystalValArray = [];
+    setGame();
+
+}
+
 
 
 
 // GAME LOGIC
 $(".crystal-image").on("click", function () {
-
-    if (yourScore === targetScore) {
-        alert("You win!!")
-        wins++;
-        $("#wins-text").text("Wins: " + wins)
-    }
-
-    else if (yourScore > targetScore) {
-        alert("You lose")
-        losses++;
-        $("#loss-text").text("Losses: " + losses);
-    }
-    else {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-
-        yourScore += parseInt(crystalValue);
+        var crystalValues = ($(this).attr("data-crystalvalue"));
+        yourScore += parseInt(crystalValues);
         $("#your-score-text").text("Your score: " + yourScore);
-    }
-
+        checkScore();
+        console.log(this);
 })
 
+
+
 // Display to screen
-targetScore();
-$("#target-score-text").text("Target Score: " + targetScore);
+setGame();
+console.log(yourScore);
+console.log(crystalValArray)
+console.log(targetScore)
